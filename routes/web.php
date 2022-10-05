884b6c46c3756bc94de63f6686301868bac2685d
Tugran Demirel <demireltugran66@gmail.com>
2022-10-06 01:04:10 +0300

    creating Admin panel work and school route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\WorkAndSchool\WorkAndSchoolController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,11 +15,20 @@ use App\Http\Controllers\Admin\AdminController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.index');
 });
 
 Auth::routes();
 
 Route::middleware(['auth'])->prefix('admin')->group(function (){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::prefix('work-and-school')->group(function (){
+        Route::get('/school', [WorkAndSchoolController::class, 'schoolIndex'])->name('admin.workandschool.school');
+        Route::get('/work', [WorkAndSchoolController::class, 'workIndex'])->name('admin.workandschool.work');
+        Route::get('/create', [WorkAndSchoolController::class, 'create'])->name('admin.workandschool.create');
+        Route::post('/store', [WorkAndSchoolController::class, 'store'])->name('admin.workandschool.store');
+        Route::get('/edit/{type}', [WorkAndSchoolController::class, 'edit'])->name('admin.workandschool.edit');
+        Route::post('/update/{type}', [WorkAndSchoolController::class, 'update'])->name('admin.workandschool.update');
+        Route::post('/delete/{type}', [WorkAndSchoolController::class, 'destroy'])->name('admin.workandschool.delete');
+    });
 });
