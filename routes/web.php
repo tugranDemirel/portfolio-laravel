028ae177ccc94d588117d4037c8cbbebcc81ea13
Tugran Demirel <demireltugran66@gmail.com>
2022-10-06 13:21:44 +0300

    Creating a route for the skill

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\WorkAndSchool\WorkAndSchoolController;
+use App\Http\Controllers\Admin\Skill\SkillController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,4 +32,22 @@ Route::middleware(['auth'])->prefix('admin')->group(function (){
         Route::post('/update/{type}', [WorkAndSchoolController::class, 'update'])->name('admin.workandschool.update');
         Route::post('/delete/{type}', [WorkAndSchoolController::class, 'destroy'])->name('admin.workandschool.delete');
     });
+
+    Route::prefix('skills')->group(function (){
+        Route::get('/', [SkillController::class, 'index'])->name('admin.skills.index');
+        Route::get('/create', [SkillController::class, 'create'])->name('admin.skills.create');
+        Route::post('/store', [SkillController::class, 'store'])->name('admin.skills.store');
+        Route::get('/edit/{type}', [SkillController::class, 'edit'])->name('admin.skills.edit');
+        Route::post('/update/{type}', [SkillController::class, 'update'])->name('admin.skills.update');
+        Route::post('/delete/{type}', [SkillController::class, 'destroy'])->name('admin.skills.delete');
+    });
+    Route::prefix('projects')->group(function (){
+        Route::get('/', [SkillController::class, 'index'])->name('admin.skills.index');
+        Route::get('/create', [SkillController::class, 'create'])->name('admin.skills.create');
+        Route::post('/store', [SkillController::class, 'store'])->name('admin.skills.store');
+        Route::get('/edit/{type}', [SkillController::class, 'edit'])->name('admin.skills.edit');
+        Route::post('/update/{type}', [SkillController::class, 'update'])->name('admin.skills.update');
+        Route::post('/delete/{type}', [SkillController::class, 'destroy'])->name('admin.skills.delete');
+    });
+
 });
