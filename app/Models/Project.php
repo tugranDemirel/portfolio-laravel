6c506bd6d665775c81115b549dfd87528c8003ea
Tugran Demirel <demireltugran66@gmail.com>
2022-10-06 17:37:00 +0300

    edit Project Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
 
 class Project extends Model
 {
@@ -21,11 +22,17 @@ class Project extends Model
     ];
     protected $casts = [
         'completed_at' => 'date',
+        'path' => 'array'
     ];
 
     public function images()
     {
         return $this->hasMany(Image::class);
+    }
+
+    public function image()
+    {
+        return $this->hasOne(Image::class);
     }
 
 }
